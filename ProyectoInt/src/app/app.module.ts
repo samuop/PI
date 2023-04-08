08d50b91593ec7b40917e3217cbbd0083d0b8aae
sam215utn <samuelocta215@gmail.com>
2023-04-07 22:23:32 -0300

    Angular

--- ProyectoInt/src/app/app.module.ts
+++ ProyectoInt/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LOGOArComponent } from './components/logoar/logoar.component';
+import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LOGOArComponent,
+    RedesSocialesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
---
+++ ProyectoInt/src/app/components/logoar/logoar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logoar',
+  templateUrl: './logoar.component.html',
+  styleUrls: ['./logoar.component.css']
+})
+export class LOGOArComponent {
+
+}
