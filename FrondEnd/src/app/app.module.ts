fb0e4701609e820be8fdde44a2198c6a23a55e74
sam215utn <samuelocta215@gmail.com>
2023-05-07 01:12:23 -0300

    Carpeta FrondEnd

--- FrondEnd/src/app/app.module.ts
+++ FrondEnd/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LOGOArComponent } from './components/logoar/logoar.component';
-import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -20,7 +19,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LOGOArComponent,
-    RedesSocialesComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
