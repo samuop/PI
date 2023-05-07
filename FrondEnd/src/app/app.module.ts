b0e2fb3c10106e7de2997dd297dab4a4850512ab
sam215utn <samuelocta215@gmail.com>
2023-05-07 18:46:28 -0300

    Add Service Token

--- FrondEnd/src/app/app.module.ts
+++ FrondEnd/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { SoftkillsComponent } from './components/softkills/softkills.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +29,8 @@ import { HttpClientModule } from '@angular/common/http';
     SoftkillsComponent,
     ProyectosComponent,
     FooterComponent,
+    HomeComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
--- FrondEnd/src/app/service/persona.service.ts
+++ FrondEnd/src/app/service/persona.service.ts
@@ -1,18 +1,41 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { persona } from '../model/persona.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonaService {
-  URL= 'http://localhost:8080/personas/';
+  URL = environment.URL + 'personas/';
+  
+  constructor(private httpClient : HttpClient) { }
 
-  constructor(private http: HttpClient) { }
+/*public getPersona(): Observable<persona>{
+    return this.http.get<persona>(this.URL+ 'traer/perfil');
+  } */
 
-  public getPersona(): Observable<persona> {
-    return this.http.get<persona>(`${this.URL}traer/pefil`);
-    
-  }
+
+public lista(): Observable<persona[]>{
+  return this.httpClient.get<persona[]>(this.URL + 'lista');
+}
+
+public detail(id: number): Observable<persona>{
+  return this.httpClient.get<persona>(this.URL + `detail/${id}`);
+}
+
+/*public save(educacion: Educacion): Observable<any>{
+  return this.httpClient.post<any>(this.URL + 'create', educacion);
+}*/
+
+public update(id: number, Persona: persona): Observable<any>{
+  return this.httpClient.put<any>(this.URL + `update/${id}`, Persona);
+}
+
+/*public delete(id: number): Observable<any>{
+  return this.httpClient.delete<any>(this.URL + `delete/${id}`);
+}*/
+
 }
